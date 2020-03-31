1e7ffbd6d1db1f871e82bb075c76cc4bd0adba5f
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-30 23:37:00 -0700

    create cell file

---
+++ GiphySearchIOS/GifCell.swift
@@ -0,0 +1,23 @@
+//
+//  GifCell.swift
+//  GiphySearchIOS
+//
+//  Created by Bo on 3/30/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GifCell: UITableViewCell {
+override func layoutSubviews() {
+    super.layoutSubviews()
+    // Add views
+}
+override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+}
+required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+}
+}
