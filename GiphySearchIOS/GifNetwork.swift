5300a1c4834506eb416bf59fbd44a34de995a9e9
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-03-31 18:47:07 -0700

    [needs review] decode NSData into Gif Object

--- GiphySearchIOS/GifNetwork.swift
+++ GiphySearchIOS/GifNetwork.swift
@@ -11,10 +11,11 @@ import Foundation
 class GifNetwork {
     let apiKey = APIKey
     /**
-     Fetches gifs from the Giphy api
-    -Parameter searchTerm: What we should query gifs of.
+    Fetches gifs from the Giphy api
+    -Parameter searchTerm: What  we should query gifs of.
+    -Returns: Optional array of gifs
     */
-    func fetchGifs(searchTerm: String) {
+    func fetchGifs(searchTerm: String, completion: @escaping (_ response: GifArray?) -> Void) {
         // Create a GET url request
         let url = URL(string: "https://api.giphy.com/v1/gifs/search?api_key=\(apiKey)&q=\(searchTerm)")!
         var request = URLRequest(url: url)
@@ -23,7 +24,13 @@ class GifNetwork {
             if let err = error {
                 print("Error fetching from Giphy: ", err.localizedDescription)
             }
-            print("Giphy Data: ", data as Any)
+            do {
+                // Decode the data into array of Gifs
+                DispatchQueue.main.async {
+                    let object = try! JSONDecoder().decode(GifArray.self, from: data!)
+                    completion(object)
+                }
+            }
         }.resume()
     }
 }
--- GiphySearchIOS/ViewController.swift
+++ GiphySearchIOS/ViewController.swift
@@ -13,54 +13,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
     
-    var network = GifNetwork()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setup()
-    }
-
-    func setup() {
-        // table view delegates
-        tableView.delegate = self
-        tableView.dataSource = self
-        // search bar delegates
-        searchBar.searchTextField.delegate = self
-        searchBar.searchTextField.placeholder = "Whats your favorite gif?"
-        searchBar.returnKeyType = .search
-    }
-    
-    /**
-    Fetches gifs based on the search term and populates tableview
-    - Parameter searchTerm: The string to search gifs of
-    */
-    func searchGifs(for searchText: String) {
-        network.fetchGifs(searchTerm: searchText)
-    }
-
-}
-
-// MARK: - Tableview functions
-extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
-    }
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "gifCell") as! GifCell
-        return cell
-    }
-}
-
-// MARK: - Search bar functions
-extension ViewController: UISearchTextFieldDelegate {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        if textField.text != nil {
-            searchGifs(for: textField.text!)
+        var network = GifNetwork()
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            setup()
         }
-        return true
+
+        func setup() {
+            // table view delegates
+            tableView.delegate = self
+            tableView.dataSource = self
+            // search bar delegates
+            searchBar.searchTextField.delegate = self
+            searchBar.searchTextField.placeholder = "Whats your favorite gif?"
+            searchBar.returnKeyType = .search
+        }
+        
+        /**
+        Fetches gifs based on the search term and populates tableview
+        - Parameter searchTerm: The string to search gifs of
+        */
+        func searchGifs(for searchText: String) {
+            network.fetchGifs(searchTerm: searchText) { results in
+                if results != nil {
+                    print(results!.gifs.count)
+                    self.tableView.reloadData()
+    
+                }
+                
+            }
+        }
+
     }
-}
+
+    // MARK: - Tableview functions
+    extension ViewController: UITableViewDelegate, UITableViewDataSource {
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 1
+        }
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 200
+        }
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "gifCell") as! GifCell
+            return cell
+        }
+    }
+
+    // MARK: - Search bar functions
+    extension ViewController: UISearchTextFieldDelegate {
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            if textField.text != nil {
+                searchGifs(for: textField.text!)
+            }
+            return true
+        }
+    }
