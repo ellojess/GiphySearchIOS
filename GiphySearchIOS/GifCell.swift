4612c879176ec787fbdc64f4e0e166fc9e7d3e4b
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-04-01 22:13:51 -0700

    edit tableview cell

--- GiphySearchIOS/GifCell.swift
+++ GiphySearchIOS/GifCell.swift
@@ -10,9 +10,24 @@ import Foundation
 import UIKit
 
 class GifCell: UITableViewCell {
+    
+    var gif: Gif?
+    // ImageView to contain our gif.
+    var gifView: UIImageView = {
+        let v = UIImageView()
+        v.contentMode = .scaleAspectFit
+        return v
+    }()
+    
 override func layoutSubviews() {
     super.layoutSubviews()
-    // Add views
+    // make sure cell has a gif object
+    if gif != nil {
+        // grab gif from object and display it inside imageview
+        let gifURL = gif!.getGifURL()
+        gifView.image = UIImage.gif(url: gifURL)
+        gifView.frame = CGRect(x: 0, y: 0, width: bounds.width, height: bounds.height)
+    }
 }
 override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
